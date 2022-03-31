297f0541825de1f875a6e6eadbe3d352da525463
Yaroslav63 <era6300@mail.ru>
2022-03-31 20:41:42 +0300

    add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿int count = 0;
+for (int i = 0; i < 5; i++)
+{
+    System.Console.WriteLine("Введите любое число:");
+    int M = Convert.ToInt32(Console.ReadLine());
+    if(M > 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine($"Количество положительных чисел: {count}");
